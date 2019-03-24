52f7250045d80c76bd1c038c41310b6f3e13cd7b
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 15:47:22 -0400

    add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Navbar extends Component {
+
+    render() {
+        return <nav class="navbar navbar-light" style={{backgroundColor: '#e3f2fd'}}>
+            Menu
+        </nav>
+    }
+}
+
+export default Navbar; 
