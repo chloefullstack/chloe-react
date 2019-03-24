d56cab93cc6a9e80d5c4a28a791bb55d3e162f59
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 18:38:09 -0400

    create homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import SignForm from './SignForm'
+import Header from './Header/Header'
+import Navbar from './Navbar'
+
+
+
+class HomePage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            userName: '',
+            password: ''
+        }
+    }
+    
+    onValueChange = (e) => {
+        console.log(e.target);
+        this.setState({
+            [e.target.name]: e.target.value
+            // userName: e.target.value
+        })
+    }
+    
+    render() {
+        let { userName, password } = this.state
+        return (
+            <div >
+                <Navbar></Navbar>
+                <Header></Header>
+                <SignForm
+                    userName={userName} password={password}
+                    onValueChange={this.onValueChange}
+                ></SignForm>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
