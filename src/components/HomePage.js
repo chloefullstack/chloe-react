70e5e7b8734eecc86e7fae2f3a96f166a304eb86
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 18:42:54 -0400

    Update SignIn route

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,32 +7,13 @@ import Navbar from './Navbar'
 
 
 class HomePage extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            userName: '',
-            password: ''
-        }
-    }
-    
-    onValueChange = (e) => {
-        console.log(e.target);
-        this.setState({
-            [e.target.name]: e.target.value
-            // userName: e.target.value
-        })
-    }
     
     render() {
-        let { userName, password } = this.state
+
         return (
             <div >
                 <Navbar></Navbar>
                 <Header></Header>
-                <SignForm
-                    userName={userName} password={password}
-                    onValueChange={this.onValueChange}
-                ></SignForm>
             </div>
         );
     }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import SignForm from './SignForm'
 
 class SignIn extends Component {
-    
+    constructor(props){
+        super(props)
+        this.state = {
+            userName: '',
+            password: ''
+        }
+    }
+
+    onValueChange = (e) => {
+        console.log(e.target);
+        this.setState({
+            [e.target.name]: e.target.value
+            // userName: e.target.value
+        })
+    }
+
     render() {
+        let { userName, password } = this.state
         return <div>
-            <SignForm></SignForm>
+            <SignForm
+                userName={userName} password={password}
+                onValueChange={this.onValueChange}>
+            </SignForm>
         </div>
     }
 }
