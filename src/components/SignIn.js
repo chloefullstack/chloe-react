495868d46c840d4a4d7e1d61032a3b169d12582d
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 18:37:50 -0400

    create about and signin pages

---
+++ src/components/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SignForm from './SignForm'
+
+class SignIn extends Component {
+    
+    render() {
+        return <div>
+            <SignForm></SignForm>
+        </div>
+    }
+}
+
+export default SignIn; // Don’t forget to use export default!
