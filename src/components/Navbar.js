da85a5c34752a791f736f1970c4d6171ad451ca5
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 18:49:31 -0400

    link route on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,17 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="/about">About</a>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Account
                         </a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Log In</a>
+                            <a className="dropdown-item" href="/signin">Log In</a>
                             <a className="dropdown-item" href="#">Sign Up</a>
                             <div className="dropdown-divider"></div>
                             <a className="dropdown-item" href="#">Log Out</a>
