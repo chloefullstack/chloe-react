940b44afa8d524eb03a9d157478190f5890b76f5
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 21:06:31 -0400

    add images and detail

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,9 @@ class Navbar extends Component {
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
+                        <a className="nav-link" href="/detail">Detail</a>
+                    </li>
+                    <li className="nav-item">
                         <a className="nav-link" href="/about">About</a>
                     </li>
                     <li className="nav-item dropdown">
