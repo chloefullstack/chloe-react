78be017d6f0581171f6e8e6d5b7fd844872681db
Patrick Jiang <vjiangcg@gmail.com>
2019-03-24 20:08:46 -0400

    add image success

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-
 import Header from './Header/Header'
 import Navbar from './Navbar'
-
+import one from '../image/one.jpg';
 
 
 class HomePage extends Component {
@@ -13,6 +12,9 @@ class HomePage extends Component {
             <div >
                 <Navbar></Navbar>
                 <Header></Header>
+                <img
+                    height={300} 
+                    src={one}/>
             </div>
         );
     }
