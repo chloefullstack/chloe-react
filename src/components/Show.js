ae7036902fd137f49836505b47a8e59cefe2dce5
chloe xu <choeqxu44@gmail.com>
2019-03-27 15:43:36 -0400

    fetch and display data from django to react

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Card }  from 'antd';
 
 import Header from './Header/Header';
 import Navbar from './Navbar';
 import ShowListView from '../containers/ShowListView';
 import Item from 'antd/lib/list/Item';
-const { Meta } = Card;
+
 class Show extends Component {
 
 
@@ -15,26 +14,19 @@ class Show extends Component {
             <Navbar></Navbar>
             <Header></Header>
             <div style={{display:'flex', justifyContent:'space-around'}}>
-                <Card hoverable style={{ width: 240 }} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-                
-                <div>
-                    {data.map(item => (
-                        <div>{item.name}</div>
-                    
-                    ))}
-                </div>
-
-                    
-                    <Meta title="Europe Street beat" description="asda"/>
-                </Card>
-                <Card hoverable style={{ width: 240 }} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-                    <Meta title="Europe Street beat" description="www.instagram.com"/>
-                </Card>
-                <Card hoverable style={{ width: 240 }} cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}>
-                    <Meta title="Europe Street beat" description="www.instagram.com"/>
-                </Card>
+                {data.map(item => (
+                    <div className="card" style={{width: '18rem'}}>
+                        <img src="..." className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Puppy Name: {item.name}</h5>
+                            <p>Sex: {item.sex}</p>
+                            <p>Age: {item.age} </p>
+                            <p className="card-text">Detail: {item.description}</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                ))}
             </div>
-            
         </div>
         
     }
