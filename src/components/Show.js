eb69ec0498ecb51a68e74165a35acc65fec392c8
chloe xu <choeqxu44@gmail.com>
2019-04-03 21:46:41 -0400

    edit

--- src/components/Show.js
+++ src/components/Show.js
@@ -12,7 +12,7 @@ class Show extends Component {
         return <div>
             <Navbar></Navbar>
             <Header></Header>
-            <a href='#'>
+            <a href='/show/createcard'>
                 <button type="button" className="btn btn-primary" style={{margin:'20px'}} >Create Puppy Info Card</button>
             </a>
             <div style={{display:'flex', justifyContent:'space-around'}}>
@@ -21,6 +21,7 @@ class Show extends Component {
                         <img src={item.profile_image} style={{height:'300px',width:'auto', maxWidth:'287px',margin:'auto'}} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">Puppy Name: {item.name}</h5>
+                            <p>{item.profile_image}</p>
                             <p>Sex: {item.sex}</p>
                             <p>Age: {item.age} </p>
                             <p className="card-text">Detail: {item.description}</p>
